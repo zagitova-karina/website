bc5736cb82e3df64c17cb0d3f1e0c591423f4662
Karina <zagitova.karina@gmail.com>
2021-03-02 21:35:06 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -130,7 +130,7 @@
 
   for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
-      this.classList.toggle("active1");
+      this.classList.toggle("active");
       var content = this.nextElementSibling;
       if (content.style.maxHeight){
         content.style.maxHeight = null;
